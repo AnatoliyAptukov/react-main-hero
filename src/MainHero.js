8659f58a820a0ef6b6d84f1a64ca64a5faf7cc70
Anatoliy Aptukov <3_kilos@mail.ru>
2024-12-15 18:34:42 +0300

    before more refactoring and beautify 15/12/2024

--- src/MainHero.js
+++ src/MainHero.js
@@ -1,139 +1,34 @@
-// import { css } from 'astroturf';
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import MainHeroGroupContext from './MainHeroGroupContext';
+import React, { useReducer, useEffect, useRef, useCallback } from 'react';
 import { useImageSize } from 'react-image-size';
-import { useResizeObserver} from "react-ext-hooks";
-// import { position, width } from 'dom-helpers';
-const maxHeight = 600;
-const aspectRatio = 900 / 600;
-const offsetFrontRatio = 1/5;
-const offsetParallaxRatio = 1/20;
-const effectParallaxOffset = 10;
-const startBackOpacity = 0.1; 
-const defaultOptions =
-  {maxHeight, aspectRatio, offsetFrontRatio, offsetParallaxRatio, effectParallaxOffset, startBackOpacity };
+import { useResizeObserver } from "react-ext-hooks";
 
-function calculateDisplayDimensions(imageSize, frameWidth) {
-  let result = { width: 0, height: 0 };
-  const width = imageSize?.width;
-  const height = imageSize?.height;
-  if (!width || !height || !frameWidth) return result;
-  const frameHeight = Math.min(maxHeight, frameWidth * aspectRatio);
-  result.height = frameHeight;
-  const estimatedWidth = (frameHeight / height) * width;
-  result.width = estimatedWidth;
-  return result;
-}
-// #region initial styles consts
-const initStyleFrame = { height: `min(${maxHeight}px,calc(100vw * ${aspectRatio}))` };
-const initStyleRB = { objectFit: 'cover', objectPosition: 'right bottom', height: '100%', position: 'absolute', bottom: 0, right: '25%', opacity: startBackOpacity, transition: 'width 0.5s ease-in 0.01s, opacity 0.5s ease-in 0.01s' };
-const initStyleLB = { objectFit: 'cover', objectPosition: 'left bottom', height: '100%', position: 'absolute', bottom: 0, left: '25%', opacity: startBackOpacity, transition: 'width 0.5s ease-in 0.01s, opacity 0.5s ease-in 0.01s' };
-const initStyleRP = { objectFit: 'cover', objectPosition: 'right bottom', height: '100%', position: 'absolute', bottom: 0, right: '25%',filter: 'hue-rotate(180deg) sepia(100%)', transition: 'width 0.5s ease-in 0.01s, right 0.5s ease-in-out 0.01s, filter 0.5s ease-in 0.01s' };
-const initStyleLP = { objectFit: 'cover', objectPosition: 'left bottom', height: '100%', position: 'absolute', bottom: 0, left: '25%',filter: 'hue-rotate(180deg) sepia(100%)', transition: 'width 0.5s ease-in 0.01s, left 0.5s ease-in-out 0.01s, filter 0.5s ease-in 0.01s' };
-const initStyleRF = { objectFit: 'cover', objectPosition: 'right bottom', height: '100%', position: 'absolute', bottom: 0, right: '25%', transition: 'width 0.5s ease-in 0s, right 0.5s ease-in-out 0.01s' };
-const initStyleLF = { objectFit: 'cover', objectPosition: 'left bottom', height: '100%', position: 'absolute', bottom: 0, left: '25%', transition: 'width 0.5s ease-in 0s, left 0.5s ease-in-out 0.01s' };
- // #endregion
+const defaultOptions = {
+  maxHeight: 600,
+  aspectRatio: 900 / 600,
+  offsetFrontRatio: 1 / 5,
+  offsetParallaxRatio: 1 / 20,
+  effectParallaxOffset: 10,
+  startBackOpacity: 0.1,
+  centerOffsetPercent: 0
+};
 
-function MainHero({ className, images, centerOffsetPercent = 0, options = defaultOptions }) {
-  const {maxHeight,aspectRatio,offsetFrontRatio, offsetParallaxRatio,effectParallaxOffset,startBackOpacity }=options
-  const relocateImages = (pointerX) => {
+function MainHero({ className, images, options }) {
+  options = { ...defaultOptions, ...options };
+  const initStyles = getInitStyles(options);
+  const sizes = useImageSizes(images);
+  const frameRef = useRef(null);
+  const [styles, dispatchStyles] = useReducer(styleReducer, initStyles);
+  const { width: watcherWidth, height: watcherHeight } = useResizeObserver({ elementRef: frameRef });
+
+  const relocateImages = useCallback((pointerX) => {
     const frameWidth = frameRef?.current?.clientWidth;
     if (!frameWidth) return;
-    const displayedDimensionsLB = calculateDisplayDimensions(sizeLB, frameWidth);
-    const displayedDimensionsRB = calculateDisplayDimensions(sizeRB, frameWidth);
-    const displayedDimensionsLP = calculateDisplayDimensions(sizeLP, frameWidth);
-    const displayedDimensionsRP = calculateDisplayDimensions(sizeRP, frameWidth);
-    const displayedDimensionsLF = calculateDisplayDimensions(sizeLF, frameWidth);
-    const displayedDimensionsRF = calculateDisplayDimensions(sizeRF, frameWidth);
-    const adjustedCenter = frameWidth / 2 - (displayedDimensionsLF.width / 100 * centerOffsetPercent);
-    const x = pointerX ?? adjustedCenter;
-    const blinkLFWidth = Math.min(Math.max(0, ((displayedDimensionsLF.width / 2) - (x - (frameWidth / 2)))), displayedDimensionsLF.width);
-    const offsetFrontX = ((displayedDimensionsLF.width / 2) - blinkLFWidth) * offsetFrontRatio;
-    const offsetParallaxX = ((displayedDimensionsLF.width / 2) - blinkLFWidth) * offsetParallaxRatio;
-    setStyleLF(prev => {
-      return {
-        ...prev,
-        left: Math.ceil(((frameWidth / 2) - (displayedDimensionsLF.width / 2)) - offsetFrontX) + 'px',
-        width: blinkLFWidth + 'px',
-      }
-    });
-    setStyleRF(prev => {
-      return {
-        ...prev,
-        right: ((frameWidth / 2) - (displayedDimensionsRF.width / 2)) + offsetFrontX + 'px',
-        width: (displayedDimensionsRF.width - blinkLFWidth) + 'px',
-      }
-    });
+    const currentStyles = calculateStyles(frameWidth, pointerX, options, sizes, initStyles);
+    dispatchStyles(currentStyles);
+  }, [images, options]);
 
-    if (sizeLP?.width) {
-      setStyleLP(prev => {
-        return {
-          ...prev,
-          left: ((frameWidth / 2) - (displayedDimensionsLP.width / 2)) - offsetParallaxX + 'px',
-          filter: (offsetFrontX + effectParallaxOffset)<0 ? '' : initStyleLP.filter,
-          // width: blinkLFWidth + (displayedDimensionsLP.width - displayedDimensionsLF.width) + 'px',
-        }
-      })
-    };
-    if (sizeRP?.width) {
-      setStyleRP(prev => {
-        return {
-          ...prev,
-          right: ((frameWidth / 2) - (displayedDimensionsRP.width / 2)) + offsetParallaxX  + 'px',
-          filter: (offsetFrontX -effectParallaxOffset) >0 ? '' : initStyleRP.filter,
-          // width: (displayedDimensionsRP.width - blinkLFWidth + (displayedDimensionsLP.width - displayedDimensionsLF.width)) + 'px',
-        }
-      })
-    };
-
-    if (sizeLB?.width) {
-      setStyleLB(prev => {
-        return {
-          ...prev,
-          left: ((frameWidth / 2) - (displayedDimensionsLP.width / 2)) + 'px',
-          opacity: (offsetFrontX + effectParallaxOffset)<0 ? 1 : initStyleRB.opacity,
-          // width: blinkLFWidth + (displayedDimensionsLP.width - displayedDimensionsLF.width) + 'px',
-        }
-      })
-    };
-    if (sizeRB?.width) {
-      setStyleRB(prev => {
-        return {
-          ...prev,
-          right: ((frameWidth / 2) - (displayedDimensionsRP.width / 2)) + 'px',
-          opacity: (offsetFrontX -effectParallaxOffset) >0 ? 1 : initStyleRB.opacity,
-          // width: (displayedDimensionsRP.width - blinkLFWidth + (displayedDimensionsLP.width - displayedDimensionsLF.width)) + 'px',
-        }
-      })
-    };
-  }
-  // #region style hooks
-  const frameRef = React.useRef(null);
-  const {width: watcherWidth, height:watcherHeight} = useResizeObserver({
-    // watchEntirePage: true,
-    elementRef:frameRef,
-  });
-  const [styleLB, setStyleLB] = React.useState(initStyleLB);
-  const [styleRB, setStyleRB] = React.useState(initStyleRB);
-  const [styleLP, setStyleLP] = React.useState(initStyleLP);
-  const [styleRP, setStyleRP] = React.useState(initStyleRP);
-  const [styleLF, setStyleLF] = React.useState(initStyleLF);
-  const [styleRF, setStyleRF] = React.useState(initStyleRF);
-
-  const [sizeLB, { loadingLB, errorLB }] = useImageSize(images.srcBackgroundLeft);
-  const [sizeRB, { loadingRB, errorRB }] = useImageSize(images.srcBackgroundRight);
-  const [sizeLP, { loadingLP, errorLP }] = useImageSize(images.srcParallaxLeft);
-  const [sizeRP, { loadingRP, errorRP }] = useImageSize(images.srcParallaxRight);
-  const [sizeLF, { loadingLF, errorLF }] = useImageSize(images.srcForegroundLeft);
-  const [sizeRF, { loadingRF, errorRF }] = useImageSize(images.srcForegroundRight);
-  // #endregion
-
-  // Observe for mount/unmount of MainHero component
   React.useEffect(() => {
-    if (!frameRef.current) return;
-    if (!sizeLF?.width && !sizeRF?.width) return; // Nothing calculate and apply effects without a front images
-
+    if (!frameRef.current || (!sizes?.frontLeft?.width && !sizes?.frontRight?.width)) return; // Nothing calculate and apply effects without a front images
     relocateImages(null);
     const handleMouseMove = (event) => {
       relocateImages(event.clientX)
@@ -144,32 +39,126 @@ function MainHero({ className, images, centerOffsetPercent = 0, options = defaul
 
     frameRef.current.addEventListener('mousemove', handleMouseMove, true);
     frameRef.current.addEventListener('mouseleave', handleMouseLeave);
-    return () => {
+    return /*Remove listeners func*/() => {
       frameRef.current.removeEventListener('mousemove', handleMouseMove, true);
     };
-  }, [frameRef, sizeLF, sizeRF, sizeLP, sizeRP]);
+  }, [frameRef, sizes?.frontLeft, sizes?.frontRight, sizes?.parallaxLeft, sizes?.parallaxRight, watcherWidth, watcherHeight]);
 
-  React.useEffect(() => {
-    relocateImages(null);
-  }, [ watcherWidth, watcherHeight])
+
   return (
-    // <MainHeroGroupContext.Provider value={null}>
     <div className={className ?? ''}
-      style={initStyleFrame}
+      style={initStyles.frame}
       ref={frameRef}
     >
       <div style={{ position: 'relative', height: '100%' }}>
-        <img style={styleRB} src={images.srcBackgroundRight} alt=''></img>
-        <img style={styleLB} src={images.srcBackgroundLeft} alt=''></img>
-        <img style={styleRP} src={images.srcParallaxRight} alt=''></img>
-        <img style={styleLP} src={images.srcParallaxLeft} alt=''></img>
-        <img style={styleRF} src={images.srcForegroundRight} alt=''></img>
-        <img style={styleLF} src={images.srcForegroundLeft} alt=''></img>
+        <img style={styles.backRight} src={images.srcBackRight} alt=''></img>
+        <img style={styles.backLeft} src={images.srcBackLeft} alt=''></img>
+        <img style={styles.parallaxRight} src={images.srcParallaxRight} alt=''></img>
+        <img style={styles.parallaxLeft} src={images.srcParallaxLeft} alt=''></img>
+        <img style={styles.frontRight} src={images.srcFrontRight} alt=''></img>
+        <img style={styles.frontLeft} src={images.srcFrontLeft} alt=''></img>
       </div>
     </div>
-    // </MainHeroGroupContext.Provider>
   );
+};
+
+function useImageSizes(images) {
+  const [backLeft] = useImageSize(images.srcBackLeft);
+  const [backRight] = useImageSize(images.srcBackRight);
+  const [parallaxLeft] = useImageSize(images.srcParallaxLeft);
+  const [parallaxRight] = useImageSize(images.srcParallaxRight);
+  const [frontLeft] = useImageSize(images.srcFrontLeft);
+  const [frontRight] = useImageSize(images.srcFrontRight);
+  const sizes = { backLeft, backRight, parallaxLeft, parallaxRight, frontLeft, frontRight };
+  return sizes;
+};
+
+function styleReducer(state, action) {
+  let newState = { ...state };
+  if (action.backLeft) newState = { ...newState, backLeft: { ...state.backLeft, ...action.backLeft } };
+  if (action.backRight) newState = { ...newState, backRight: { ...state.backRight, ...action.backRight } };
+  if (action.parallaxLeft) newState = { ...newState, parallaxLeft: { ...state.parallaxLeft, ...action.parallaxLeft } };
+  if (action.parallaxRight) newState = { ...newState, parallaxRight: { ...state.parallaxRight, ...action.parallaxRight } };
+  if (action.frontLeft) newState = { ...newState, frontLeft: { ...state.frontLeft, ...action.frontLeft } };
+  if (action.frontRight) newState = { ...newState, frontRight: { ...state.frontRight, ...action.frontRight } };
+  return newState;
+};
+
+function calculateStyles(frameWidth, pointerX, options, sizes, initStyles) {
+
+  const displaySizes = getDisplaySizes(sizes, frameWidth, options);
+
+  const adjustedCenter = frameWidth / 2 - (displaySizes.frontLeft.width / 100 * options.centerOffsetPercent);
+  const x = pointerX ?? adjustedCenter;
+  // const x = 0;
+  const blinkLFWidth = Math.min(Math.max(0, ((displaySizes.frontLeft.width / 2) - (x - (frameWidth / 2)))), displaySizes.frontLeft.width);
+  const offsetFrontX = ((displaySizes.frontLeft.width / 2) - blinkLFWidth) * options.offsetFrontRatio;
+  const offsetParallaxX = ((displaySizes.frontLeft.width / 2) - blinkLFWidth) * options.offsetParallaxRatio;
+  // console.log(frameWidth, 'frameWidth');
+  // console.log(displaySizes.frontRight.width, 'displaySizes.frontRight.width');
+  // console.log(offsetFrontX, 'offsetFrontX');
+  const styles = {
+    frontLeft: {
+      left: Math.ceil(((frameWidth / 2) - (displaySizes.frontLeft.width / 2)) - offsetFrontX) + 'px',
+      width: blinkLFWidth  + 'px',
+    },
+    frontRight: {
+      right: Math.floor(((frameWidth / 2) - (displaySizes.frontRight.width / 2)) + offsetFrontX) + 'px',
+      width: (displaySizes.frontRight.width - blinkLFWidth) + 'px',
+    },
+    parallaxLeft: sizes?.parallaxLeft?.width ? {
+      left: ((frameWidth / 2) - (displaySizes.parallaxLeft.width / 2)) - offsetParallaxX + 'px',
+      filter: (offsetFrontX + options.effectParallaxOffset) < 0 ? '' : initStyles.parallaxLeft.filter,
+    } : null,
+    parallaxRight: sizes?.parallaxRight?.width ? {
+      right: ((frameWidth / 2) - (displaySizes.parallaxRight.width / 2)) + offsetParallaxX + 'px',
+      filter: (offsetFrontX - options.effectParallaxOffset) > 0 ? '' : initStyles.parallaxRight.filter,
+    } : null,
+    backLeft: sizes?.backLeft?.width ? {
+      left: ((frameWidth / 2) - (displaySizes.backLeft.width / 2)) + 'px',
+      opacity: (offsetFrontX + options.effectParallaxOffset) < 0 ? 1 : initStyles.backLeft.opacity,
+    } : null,
+    backRight: sizes?.backRight?.width ? {
+      right: ((frameWidth / 2) - (displaySizes.backRight.width / 2)) + 'px',
+      opacity: (offsetFrontX - options.effectParallaxOffset) > 0 ? 1 : initStyles.backRight.opacity,
+    } : null,
+  }
+  return styles;
 }
 
+function getInitStyles(options) {
+
+  // #region initial styles consts
+  const frame = { height: `min(${options.maxHeight}px,calc(100vw / ${options.aspectRatio}))`, overflowX: 'clip' };
+  const backRight = { objectFit: 'cover', objectPosition: 'right bottom', height: '100%', position: 'absolute', bottom: 0, right: '25%', opacity: options.startBackOpacity, transition: 'width 0.5s ease-in 0.01s, opacity 0.5s ease-in 0.01s' };
+  const backLeft = { objectFit: 'cover', objectPosition: 'left bottom', height: '100%', position: 'absolute', bottom: 0, left: '25%', opacity: options.startBackOpacity, transition: 'width 0.5s ease-in 0.01s, opacity 0.5s ease-in 0.01s' };
+  const parallaxRight = { objectFit: 'cover', objectPosition: 'right bottom', height: '100%', position: 'absolute', bottom: 0, right: '25%', filter: 'hue-rotate(180deg) sepia(100%)', transition: 'width 0.5s ease-in 0.01s, right 0.5s ease-in-out 0.01s, filter 0.5s ease-in 0.01s' };
+  const parallaxLeft = { objectFit: 'cover', objectPosition: 'left bottom', height: '100%', position: 'absolute', bottom: 0, left: '25%', filter: 'hue-rotate(180deg) sepia(100%)', transition: 'width 0.5s ease-in 0.01s, left 0.5s ease-in-out 0.01s, filter 0.5s ease-in 0.01s' };
+  const frontRight = { objectFit: 'cover', objectPosition: 'right bottom', height: '100%', position: 'absolute', bottom: 0, right: '25%', transition: 'width 0.5s ease-in 0s, right 0.5s ease-in-out 0.01s' };
+  const frontLeft = { objectFit: 'cover', objectPosition: 'left bottom', height: '100%', position: 'absolute', bottom: 0, left: '25%', transition: 'width 0.5s ease-in 0s, left 0.5s ease-in-out 0.01s' };
+  return { frame, backRight, backLeft, parallaxRight, parallaxLeft, frontRight, frontLeft };
+};
+
+function getDisplaySizes(sizes, frameWidth, options) {
+  const backLeft = calculateDisplaySize(sizes.backLeft, frameWidth, options);
+  const backRight = calculateDisplaySize(sizes.backRight, frameWidth, options);
+  const parallaxLeft = calculateDisplaySize(sizes.parallaxLeft, frameWidth, options);
+  const parallaxRight = calculateDisplaySize(sizes.parallaxRight, frameWidth, options);
+  const frontLeft = calculateDisplaySize(sizes.frontLeft, frameWidth, options);
+  const frontRight = calculateDisplaySize(sizes.frontRight, frameWidth, options);
+  return { backLeft, backRight, parallaxLeft, parallaxRight, frontLeft, frontRight };
+};
+
+function calculateDisplaySize(imageSize, frameWidth, options) {
+  let result = { width: 0, height: 0 };
+  const width = imageSize?.width;
+  const height = imageSize?.height;
+  if (!width || !height || !frameWidth) return result;
+  const frameHeight = Math.min(options.maxHeight, frameWidth / options.aspectRatio);
+ 
+  result.height = frameHeight;
+  result.width = (frameHeight / height) * width;
+  return result;
+};
 
 export default MainHero;
